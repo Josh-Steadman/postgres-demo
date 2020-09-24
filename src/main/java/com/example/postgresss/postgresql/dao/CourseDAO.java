82ef84a01c976095d2c6df098f5ce988fa727703
joshSteadman <josh.steadman15@gmail.com>
2020-09-24 12:48:45 +0100

    database filler service

--- src/main/java/com/example/postgresss/postgresql/dao/CourseDAO.java
+++ src/main/java/com/example/postgresss/postgresql/dao/CourseDAO.java
@@ -3,4 +3,5 @@ package com.example.postgresss.postgresql.dao;
 import com.example.postgresss.postgresql.entities.Course;
 import org.springframework.data.repository.CrudRepository;
 import java.util.UUID;
+
 public interface CourseDAO extends CrudRepository<Course, UUID>{}
