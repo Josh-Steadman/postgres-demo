2a957a2b70cb50eb725f4844c49810635368e87b
joshSteadman <josh.steadman15@gmail.com>
2020-09-24 16:22:48 +0100

    flyway init

--- src/main/java/com/example/postgresss/PostgresssApplication.java
+++ src/main/java/com/example/postgresss/PostgresssApplication.java
@@ -2,7 +2,10 @@ package com.example.postgresss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -12,4 +15,15 @@ public class PostgresssApplication {
 		SpringApplication.run(PostgresssApplication.class, args);
 	}
 
+	@Configuration
+	static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests().anyRequest().authenticated()
+					.and()
+					.oauth2ResourceServer().jwt();
+		}
+	}
 }
